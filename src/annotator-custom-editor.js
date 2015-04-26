2763f3d5d9479d8c514121c8ecdafd8151123e28
Tyler Burton <btburton42@gmail.com>
2015-04-26 12:53:01 -0700

    Loading new 2.0 Annotator with default UI   • Retrieve JSON payload from TT Backend via Promise

--- src/annotator-custom-editor.js
+++ src/annotator-custom-editor.js
@@ -1,27 +1,45 @@
-Annotator.Plugin.CustomEditor = class CustomEditor extends Annotator.Plugin{
-  pluginInit(){
-    this.annotator
-      .subscribe('beforeAnnotationCreated', annotation => {
-        this.customSetup(annotation);
+function annotatorCustomEditor(options){
+  return {
+    start: function() {
+      this.options = options;
+      this.getDataPayload()
+    },
+
+    beforeAnnotationCreated(annotation){
+      // TODO: explore how annotator instantiates its UI and build from there.
+      console.log('beforeAnnotationCreated')
+    },
+
+    // ajax json promise function to retrieve data
+    getDataPayload() {
+      return this.jsonPromise(this.options.endpoint)
+        .then((value) => {
+            this.configureData(value);
+          }
+        )
+    },
+
+    // method to model our raw data payload
+    // @param value [object] API payload returned via AJAX
+    //
+    configureData(value){
+      // TODO: establish how we want to configure the data
+      this.data = {
+        next: value.next,
+        previous: value.previous,
+        glossary: value.results[0].analysis_type.glossary
+      }
+    },
+
+     // helper promise function for getDataPayload()
+     // @param url [string] data endpoint to resolve
+     //
+    jsonPromise(url) {
+      return new Promise((resolve, reject) => {
+        let json = $.ajax(url);
+        resolve(json)
       })
-      .subscribe('annotationCreated', annotation => {
-        this.newCustomMethod(annotation);
-      })
-      .subscribe('annotationUpdated', annotation => {
-        this.newCustomMethod(annotation);
-      })
-      .subscribe('annotationDeleted', annotation => {
-        this.newCustomMethod(annotation);
-      })
+    }
 
   }
-
-  customSetup(annotation){
-    console.log(annotation);
-  }
-
-  newCustomMethod(annotation){
-    console.log(annotation);
-  }
-
 }
