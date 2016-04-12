66c350ac67e5930bca1e91d9eaab1067d8d1f2c1
Arjun Mehta <arjunmehta94@berkeley.edu>
2016-04-12 15:25:07 -0700

    creates updateAnswer type and action creator

--- app/actions/actionTypes.js
+++ app/actions/actionTypes.js
@@ -2,3 +2,4 @@ export const ADD_HIGHLIGHT = 'ADD_HIGHLIGHT';
 export const ACTIVATE_TOPIC = 'ACTIVATE_TOPIC';
 export const NEW_ARTICLE = 'NEW_ARTICLE';
 export const NEW_QUESTIONS = 'NEW_QUESTIONS';
+export const UPDATE_ANSWER = 'UPDATE_ANSWER';
