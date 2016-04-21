38b4d9399c26e095c5466c1609816fb34185be26
Arjun Mehta <arjunmehta94@berkeley.edu>
2016-04-20 17:41:54 -0700

    improves answer section of quizreducer, need to still work on this

--- app/reducers/quizReducers.js
+++ app/reducers/quizReducers.js
@@ -1,4 +1,4 @@
-import { NEW_QUESTIONS } from '../actions/actionTypes';
+import { NEW_QUESTIONS, UPDATE_ANSWER } from '../actions/actionTypes';
 import tmpQuestions from '../assets/tmpQuestions.json';
 
 const initialState = {
@@ -9,6 +9,9 @@ export default function quizReducer(state = initialState, action) {
   switch (action.type) {
     case NEW_QUESTIONS:
       return Object.assign({}, state, { questions: action.questions });
+    case UPDATE_ANSWER:
+    	console.log(action.answers);
+    	return Object.assign({}, state, { answers: action.answers });
     default:
       return state;
   }
